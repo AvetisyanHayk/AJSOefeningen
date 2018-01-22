4dd00a50ece0c935c9e1afff56bcf83a7b8ba9b5
MOBILE-OVGFSFC\hayk <hayk.avetisyan@student.howest.be>
2018-01-22 13:16:45 +0100

    update .gitignore + assets

---
+++ 6-Niko/assets/data.js
@@ -0,0 +1,27 @@
+var SERIE_DATA = [
+    {
+        name: "original",
+        colors: [
+            {code: 100, name: "Cream"},
+            {code: 101, name: "White"}
+        ]
+    },
+    {id: 2, name: "intense"},
+    {id: 3, name: "pure"}
+];
+
+var DEVICE_DATA = [
+    {
+        name: "switch",
+        devices: [
+            {
+                type: 17033100,
+                name: "Sokkel wandcontactdoos",
+                suffix: 33100,
+                device: "Wandcontactdoos met penaarde en kinderbeveiliging",
+                codes: [100, 101, 120, 121, 122, 123],
+                accessories: [170999999, 170846546, 190321546]
+            }
+        ]
+    }
+];
